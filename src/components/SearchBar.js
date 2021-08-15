097b544cb23da7b800b4be92c844ebd3de9bd34d
Igor Veyner <igorveyner95@gmail.com>
2021-08-15 13:08:49 -0400

    create searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,53 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { useState, useRef } from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native'
 
 const SearchBar = () => {
+  const [text, onChangeText] = useState("")
+  const input = useRef(null)
+
+  const handlePress = () => {
+    Keyboard.dismiss()
+  }
+
   return (
-    <View style={styles.SearchBar}>
-      <Text>This is a searchbar</Text>
-    </View>
+    <TouchableWithoutFeedback
+      style={styles.SearchBarContainer}
+      onPress={handlePress}
+    >
+      <View 
+        style={styles.SearchBarContainer}  
+      >
+        <TextInput
+          style={styles.SearchBar}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder={"Search"}
+          ref={input}
+          onBlur={Keyboard.dismiss}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
 const styles = StyleSheet.create({
-  SearchBar: {
+  SearchBarContainer: {
     flex: 1,
     backgroundColor: '#ddd',
     width: '100%',
     alignItems: 'center',
-    justifyContent: 'center',
-  }
+    justifyContent: 'flex-start',
+  },
+
+  SearchBar: {
+    backgroundColor: 'white',
+    marginVertical: 24,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    width: '90%',
+    borderRadius: 20,
+  },
 })
 
 export default SearchBar
